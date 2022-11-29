0d57c6e38e36c19134d0569fe47eb3308fb34dd1
pjgowtham <pjgowtham@gmail.com>
2022-11-29 15:49:17 +0530

    RMX3461: Add recovery fstab with SDCard support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,7 @@ TARGET_SCREEN_DENSITY := 450
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
+TARGET_RECOVERY_FSTAB += $(DEVICE_PATH)/init/recovery.fstab
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
