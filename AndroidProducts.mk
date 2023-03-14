050a606d78dbc739b599dd3dd82175ce9f93a52c
pjgowtham <pjgowtham@gmail.com>
2023-03-14 18:41:44 +0530

    ice: Initial rebrand for realme Q3S/realme 9 SE

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonade.mk
+    $(LOCAL_DIR)/lineage_ice.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonade-user \
-    lineage_lemonade-userdebug \
-    lineage_lemonade-eng
+    lineage_ice-user \
+    lineage_ice-userdebug \
+    lineage_ice-eng
